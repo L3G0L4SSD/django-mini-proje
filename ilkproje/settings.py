09f49e0c8881067b5c8dd5ec4d47c6b5cd32bb39
altaygnbtn <altaygunbatan@hotmail.com>
2025-04-18 22:11:21 +0300

    authentication is established via user database

--- ilkproje/settings.py
+++ ilkproje/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'ecommerce_app'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'ilkproje.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
