7f75a6bde3f8f16cd4825a813a07ac42a38cea95
altaygnbtn <altaygunbatan@hotmail.com>
2025-05-07 20:36:13 +0300

    details page added

--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -7,8 +7,8 @@ app_name = 'ecommerce'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
     path('products/', views.products, name='products'),
     path('add_product/', views.add_product, name='add_product'),
