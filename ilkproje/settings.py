a6821c658139b5e5bdf7e49f90a0b74af1a1f3bb
BartuRhymlhold <bartuturkkan@gmail.com>
2025-05-16 19:52:25 +0300

    render deployment

--- ilkproje/settings.py
+++ ilkproje/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,10 +81,11 @@ WSGI_APPLICATION = 'ilkproje.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),  # fallback if no DATABASE_URL set
+        conn_max_age=600,  # keep connections alive
+        ssl_require=True
+    )
 }
 
 
