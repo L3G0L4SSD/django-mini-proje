57032f980e70898ee9b043ba4652e4a81c752f14
altaygnbtn <altaygunbatan@hotmail.com>
2025-05-19 11:53:41 +0300

    added default_file_storage

--- ilkproje/settings.py
+++ ilkproje/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["ecommerce-myshop-hxhf.onrender.com"]
+ALLOWED_HOSTS = ["ecommerce-myshop-hxhf.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -149,3 +149,4 @@ if not DEBUG:
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
