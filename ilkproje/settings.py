58fb52ade45a7766477d67c54dc59fe3d54be59c
altaygnbtn <altaygunbatan@hotmail.com>
2025-05-19 12:01:03 +0300

    added default_file_storage

--- ilkproje/settings.py
+++ ilkproje/settings.py
@@ -149,4 +149,4 @@ if not DEBUG:
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+   
